c9b58f262da7c9bb9b12b1016f75e969ba4db832
codecShivam <codecshivam@gmail.com>
2023-07-13 03:10:41 +0530

    added logo component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import {Outlet} from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
 
- const Home = () => {
+const Home = () => {
   return (
-    <div className=' text-black'>Home
-
-        <Outlet />
-    </div>
-  )
-}
+    <main className="w-full h-full flex flex-col first-letter content-center items-center relative text-white font-nunito">
+      <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+      <Logo />
+      <Outlet />
+    </main>
+  );
+};
 
 export default Home;
