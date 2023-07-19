e64f1b7e760b10211e3c0f3928e5b1e67b1a56e2
Shivam Yadav <codecshivam@gmail.com>
2023-07-19 22:04:19 +0000

    fixed table overflow

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,21 +7,20 @@ const Table = () => {
     let { cryptoData } = useContext(CryptoContext);
 
     return (
-        <div className="flex w-[80%] flex-col mt-9 border border-gray-100 rounded">
+        <div className="flex flex-col mt-9 border border-gray-100 rounded">
             {cryptoData ?
                 <table className="w-full table-auto">
                     <thead className="capitalize text-base text-gray-100 border-b border-gray-100">
                         <tr>
-                            <th className="py-1">asset</th>
-                            <th className="py-1">name</th>
-                            <th className="py-1">price</th>
-                            <th className="py-1">totla volume</th>
-                            <th className="py-1">market cap change</th>
-                            <th className="py-1">1H</th>
-                            <th className="py-1">24H</th>
-                            <th className="py-1">7D</th>
-
-                            <th className="py-1">image</th>
+                            <th className="py-1 px-12">asset</th>
+                            <th className="py-1 px-12">name</th>
+                            <th className="py-1 px-12">price</th>
+                            <th className="py-1 px-12">totla volume</th>
+                            <th className="py-1 px-12">market cap change</th>
+                            <th className="py-1 px-12">1H</th>
+                            <th className="py-1 px-12">24H</th>
+                            <th className="py-1 px-12">7D</th>
+                            <th className="py-1 px-12">image</th>
 
                         </tr>
                     </thead>
@@ -29,11 +28,11 @@ const Table = () => {
                         {cryptoData.map((data,index) => {
                             return (
                                 <tr key={data.id} className="text-center text-base text-white capitalize  border-b borde-gray-100 hover:bg-gray-200 last:border-b-0 ">
-                                    <td className="py-4">{index}</td>
+                                    <td className="py-4">{index+1}</td>
                                     <td className="py-4">{data.name}</td>
                                     <td className="py-4">price</td>
                                     <td className="py-4">{data.total_volume}</td>
-                                    <td className="py-4">market cap change</td>
+                                    <td className="py-4">{data.market_cap}</td>
                                     <td className="py-4">{data.price_change_percentage_1h_in_currency}</td>
                                     <td className="py-4">{data.price_change_percentage_24h_in_currency}</td>
                                     <td className="py-4">{data.price_change_percentage_7d_in_currency}</td>
