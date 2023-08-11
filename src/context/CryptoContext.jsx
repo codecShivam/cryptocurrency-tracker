ebbb564dc09d8c7da0a64f85ba88fdd767fdd2c3
Shivam Yadav <codecshivam@gmail.com>
2023-08-11 19:35:14 +0000

    added image fav icon

--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -10,8 +10,7 @@ export const CryptoProvider = ({ children }) => {
       const data = await fetch(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
       )
-        .then((res) => res.json())
-        .then((json) => json);
+        .then((res) => res.json()).then((json) => json);
       console.log(data);
       setCryptoData(data)
     } catch (error) {
