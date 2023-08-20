ff88db6868c63da0b89d641c3ac21ef614535e41
codecShivam <codecshivam@gmail.com>
2023-08-20 13:16:50 +0530

    used debounce function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,42 +1,55 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useContext } from "react";
 import searchIcon from "../assets/search-icon.svg";
-import { useContext } from "react";
 import { CryptoContext } from "../context/CryptoContext";
+import debounce from "lodash.debounce";
 
-const Search = () => {
+const SearchInput = ({ handleSearch }) => {
   const [searchText, setSearchText] = useState(""); // search state
-  let { getSearchResult } = useContext(CryptoContext);
+
   const handleInput = (e) => {
-     e.preventDefault();
-     let query = e.target.value;
+    const query = e.target.value;
     setSearchText(query);
-    getSearchResult(query);
+    handleSearch(query);
   };
 
   return (
     <div>
-      <form 
-      onChange={handleInput}
-      className="w-96 relative flex items-center ml-7 font-nunito ">
-
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-96 relative flex items-center ml-7 font-nunito"
+      >
         <input
-          typeof="text"
-          name="search"
-          className=" w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan"
           type="text"
+          name="search"
+          className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan"
           placeholder="Search here..."
+          value={searchText}
+          onChange={handleInput}
         />
-        <button type="submit" className=" absolute right-1 cursor-pointer">
+        <button type="submit" className="absolute right-1 cursor-pointer">
           <img src={searchIcon} className="w-full h-auto" alt="search" />
         </button>
       </form>
       {searchText.length > 0 ? (
-        <ul className="absolute top-11 right-0 w-full  h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md">
+        <ul className="absolute top-11 right-0 w-full h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md">
           <li>bitcoin</li>
           <li>ethereum</li>
         </ul>
       ) : null}
+    </div>
+  );
+};
+
+const Search = () => {
+  const { getSearchResult } = useContext(CryptoContext);
+
+  const debounceFunc = debounce(function (val) {
+    getSearchResult(val);
+  }, 1000); // Adjust the debounce time according to your needs
+
+  return (
+    <div>
+      <SearchInput handleSearch={debounceFunc} />
     </div>
   );
 };
