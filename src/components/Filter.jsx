bab9a0e4f086bcdbf2b1047ec431a8eddb541a5e
codecShivam <codecshivam@gmail.com>
2023-08-27 17:39:46 +0530

    integrating pagination feat

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import Search from "./Search";
 import submitIcon from "../assets/submit-icon.svg";
 import selectIcon from "../assets/select-icon.svg";
-import { useContext, useRef } from "react";
-import { CryptoContext } from "../context/CryptoContext";
-
-const Filter = () => {
-  let { setCurrency ,setSortBy } = useContext(CryptoContext);
+import {CryptoContext} from "./../context/CryptoContext";
+const Filters = () => {
+  let { setCurrency, setSortBy, resetFunction } = useContext(CryptoContext);
   const currencyRef = useRef(null);
 
   const handleCurrencySubmit = (e) => {
@@ -16,16 +14,20 @@ const Filter = () => {
     currencyRef.current.value = "";
   };
 
-  const handleSort = (e) => { 
+  const handleSort = (e) => {
     e.preventDefault();
     let val = e.target.value;
     setSortBy(val);
-  }
+  };
 
   return (
-    <div className="w-full h-12 border-2 bg-gray-100 rounded-lg flex items-center justify-between relative">
+    <div
+      className="w-full h-12 border-2 border-gray-100 rounded-lg
+    flex items-center justify-between relative
+    "
+    >
       <Search />
-      <div className="mr-7 ">
+      <div className="flex mr-7">
         <form
           className="relative flex items-center font-nunito
           mr-12
@@ -54,8 +56,8 @@ const Filter = () => {
             <img src={submitIcon} alt="submit" className="w-full h-auto" />
           </button>
         </form>
-      </div>
-      <label className="relative flex justify-center items-center">
+
+        <label className="relative flex justify-center items-center">
           <span className="font-bold mr-2">sort by: </span>
           <select
             name="sortby"
@@ -81,8 +83,32 @@ const Filter = () => {
          "
           />
         </label>
+        <button
+          className="w-[2rem] ml-4 hover:scale-110 transition-all transition-ease
+        relative
+        "
+          onClick={resetFunction}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            className="w-full h-full fill-cyan"
+            style={{
+              msTransform: "rotate(360deg)",
+              WebkitTransform: "rotate(360deg)",
+              transform: "rotate(360deg)",
+            }}
+          >
+            <path d="M12 16c1.671 0 3-1.331 3-3s-1.329-3-3-3-3 1.331-3 3 1.329 3 3 3z" />
+            <path d="M20.817 11.186a8.94 8.94 0 0 0-1.355-3.219 9.053 9.053 0 0 0-2.43-2.43 8.95 8.95 0 0 0-3.219-1.355 9.028 9.028 0 0 0-1.838-.18V2L8 5l3.975 3V6.002c.484-.002.968.044 1.435.14a6.961 6.961 0 0 1 2.502 1.053 7.005 7.005 0 0 1 1.892 1.892A6.967 6.967 0 0 1 19 13a7.032 7.032 0 0 1-.55 2.725 7.11 7.11 0 0 1-.644 1.188 7.2 7.2 0 0 1-.858 1.039 7.028 7.028 0 0 1-3.536 1.907 7.13 7.13 0 0 1-2.822 0 6.961 6.961 0 0 1-2.503-1.054 7.002 7.002 0 0 1-1.89-1.89A6.996 6.996 0 0 1 5 13H3a9.02 9.02 0 0 0 1.539 5.034 9.096 9.096 0 0 0 2.428 2.428A8.95 8.95 0 0 0 12 22a9.09 9.09 0 0 0 1.814-.183 9.014 9.014 0 0 0 3.218-1.355 8.886 8.886 0 0 0 1.331-1.099 9.228 9.228 0 0 0 1.1-1.332A8.952 8.952 0 0 0 21 13a9.09 9.09 0 0 0-.183-1.814z" />
+            <path fill="rgba(0, 0, 0, 0)" d="M0 0h24v24H0z" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Filter;
+export default Filters;
--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -8,11 +8,25 @@ export const CryptoProvider = ({ children }) => {
   const [coinSearch, setCoinSearch] = useState(""); // search state
   const [currency, setCurrency] = useState("usd"); // currency state
   const [sortBy, setSortBy] = useState("market_cap_desc"); // sort state
+  const [page, setPage] = useState(1); // page state
+  const [totalPages, setTotalPages] = useState(250); // total pages state
 
   const getCryptoData = async () => {
     try {
       const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+        `https://api.coingecko.com/api/v3/coins/list`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+      console.log(data);
+      setTotalPages(data.length);
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
       )
         .then((res) => res.json())
         .then((json) => json);
@@ -34,9 +48,17 @@ export const CryptoProvider = ({ children }) => {
       console.log(error);
     }
   };
+
+  const resetFunction = () => {
+    setPage(1);
+    setCoinSearch("");
+    setSortBy("market_cap_desc");
+    setCurrency("usd");
+  };
+
   useLayoutEffect(() => {
     getCryptoData();
-  }, [coinSearch, currency, sortBy]);
+  }, [coinSearch, currency, sortBy, page]);
 
   return (
     <CryptoContext.Provider
@@ -49,7 +71,12 @@ export const CryptoProvider = ({ children }) => {
         currency,
         setCurrency,
         sortBy, 
-        setSortBy
+        setSortBy,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages,
+        resetFunction,
       }}
     >
       {children}
